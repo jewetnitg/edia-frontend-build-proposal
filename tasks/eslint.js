cf39bb3148c29cb8981caceb09b13badce308c0b
Rik Hoffbauer <Rikhoffbauer@gmail.com>
2016-02-02 20:58:52 +0100

    documentend eslint gulp task

--- tasks/eslint.js
+++ tasks/eslint.js
@@ -1,6 +1,11 @@
 var eslint = require('gulp-eslint');
 
 module.exports = function (gulp) {
+  /**
+   * Lints the javascript
+   * @name eslint
+   * @memberof build.tasks
+   */
   gulp.task('eslint', function () {
     return gulp.src(['./src/js/**/*.js'])
       .pipe(eslint())
