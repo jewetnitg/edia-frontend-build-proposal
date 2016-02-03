e5b1b2d7d3d3137954f69673cf66430196bb51e8
Rik Hoffbauer <Rikhoffbauer@gmail.com>
2016-02-03 14:41:17 +0100

    refactored the dependo gulp task

--- tasks/dependo.js
+++ tasks/dependo.js
@@ -1,25 +1,33 @@
 var Dependo = require("dependo");
 var fs = require('fs');
+var path = require('path');
 var mkdirp = require('mkdirp');
 
 module.exports = function (gulp) {
+
+  var options = {
+    src: './src',
+    dst: './build/docs/dependency_graph.html',
+    dependoOptions: {
+      format: 'cjs',
+      exclude: 'node_modules',
+    }
+  };
+
   /**
    * Generates a dependency graph using the dependo npm package
    * @name dependo
    * @memberof build.tasks
    */
   return gulp.task('dependo', function (cb) {
-    var dependo = new Dependo('./src', {
-      format: 'cjs',
-      exclude: 'node_modules',
-    });
+    var dependo = new Dependo(options.src, options.dependoOptions);
 
-    mkdirp('./build/docs/', function (err) {
+    mkdirp(path.dirname(options.dst), function (err) {
       if (err) {
         throw new Error('Dependo failed to ensure it\'s destination directory.');
       }
-      
-      fs.writeFile('./build/docs/dependency_graph.html', dependo.generateHtml(), function (err) {
+
+      fs.writeFile(options.dst, dependo.generateHtml(), function (err) {
         if (err) {
           throw new Error('Dependo failed to write file to disk.');
         } else {
